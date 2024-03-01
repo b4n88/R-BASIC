afa990a2d3fc1129acb299f92a3a8288738c166c
Albani <50041403+b4n88@users.noreply.github.com>
2024-03-02 05:32:58 +0700

    Create Rmarkdowntheme.r

---
+++ Rmarkdowntheme.r
@@ -0,0 +1,12 @@
+prity HTML
+---
+title: Nineteen Years Later
+author: Harry Potter
+date: July 31, 2016
+output:
+  prettydoc::html_pretty:
+    theme: cayman
+    highlight: github
+---
+https://prettydoc.statr.me/index.html
+Options code_folding, code_download and toc_float are not applicable.
