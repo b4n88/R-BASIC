5cd08a3913a35bb1c06709f2327ce7ea8d27f2e8
Albani <50041403+b4n88@users.noreply.github.com>
2024-03-04 08:27:00 +0800

    Update Markdown.r

--- Markdown.r
+++ Markdown.r
@@ -4,7 +4,7 @@ author: "Albani"
 date: "2024-03-03"
 output: 
   rmdformats::downcute:
-    code_cfolding: show
+    code_folding: show
     code_download: yes
     self_contained: true
     thumbnails: false
