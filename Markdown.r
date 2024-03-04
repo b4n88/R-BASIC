d58d29aaa89f4396e5e34d6499b41a92755785eb
Albani <50041403+b4n88@users.noreply.github.com>
2024-03-04 10:46:14 +0800

    Update Markdown.r

--- Markdown.r
+++ Markdown.r
@@ -3,13 +3,14 @@ title: "Uji Perbedaan data"
 author: "Albani"
 date: "2024-03-03"
 output: 
-  rmdformats::downcute:
+   rmdformats::downcute:
     code_folding: show
     code_download: yes
     self_contained: true
-    thumbnails: false
     lightbox: true
-pkgdown:
-  as_is: true
+    default_style: "dark"
+    highlight: NULL
+    downcute_theme: "chaos"
+    use_bookdown: false
 ---
 
