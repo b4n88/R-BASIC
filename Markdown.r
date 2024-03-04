d66842181ae56c19a2e010997b111d36b8398aaf
Albani <50041403+b4n88@users.noreply.github.com>
2024-03-04 21:20:30 +0700

    Update Markdown.r

--- Markdown.r
+++ Markdown.r
@@ -8,9 +8,9 @@ output:
     code_download: yes
     self_contained: true
     lightbox: true
-    default_style: "dark"
+    default_style: "dark", "light"
     highlight: NULL
-    downcute_theme: "chaos"
-    use_bookdown: false
+    downcute_theme: "chaos", "default"
+    use_bookdown: false # jika true maka akan membuat Bab (5, 5.1, 5.2)
 ---
 
