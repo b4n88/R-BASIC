503b9a17cf1a62bce8fcaec8ebe0b9c3ae7c17cf
Albani <50041403+b4n88@users.noreply.github.com>
2024-03-17 11:52:09 +0700

    Create Markdown_htmldocumen.r

---
+++ Markdown_htmldocumen.r
@@ -0,0 +1,12 @@
+---
+title: "This is a Default Themes YAML"
+author: "Type your name here"
+date: "`r Sys.Date()`"
+output: 
+  html_document:
+    theme: cerulean
+    code_folding
+    toc: yes
+    toc_float:
+      collapsed: true
+---
