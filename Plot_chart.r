5f709905e21de28384bc55b136f6ed1f581cc748
Albani <50041403+b4n88@users.noreply.github.com>
2024-03-19 21:26:30 +0700

    Update Plot_chart.r

--- Plot_chart.r
+++ Plot_chart.r
@@ -3,3 +3,6 @@ library(esquisse)
 esquisser(iris) #ganti "iris" sesuai data yang ingin di butkan Plot
 Note : Membuat grafik dengan drag n drop
 ------------------------------------------------------------------------------------------------------------------------------
+# Membuat dengan GGplot
+library(ggplot2)
+
