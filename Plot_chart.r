362e5c01346553628dda0a38beacdb36cc02f557
Albani <50041403+b4n88@users.noreply.github.com>
2024-03-19 21:25:52 +0700

    Create Plot_chart.r

---
+++ Plot_chart.r
@@ -0,0 +1,5 @@
+# membuat Chart dengan Esquisse
+library(esquisse)
+esquisser(iris) #ganti "iris" sesuai data yang ingin di butkan Plot
+Note : Membuat grafik dengan drag n drop
+------------------------------------------------------------------------------------------------------------------------------
